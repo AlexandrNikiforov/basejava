fdb8c6f6c343234ff478d72d5794e231a01255bc
alexandr.nikiforov <nikiforov.san.sanich@gmail.com>
2021-03-22 22:42:57 +0300

    Add clear, save, get method implementations to ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,16 +5,43 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (Resume resume : this.storage) {
+            resume = null;
+        }
     }
 
     void save(Resume r) {
+        for (int i = 0; i < this.storage.length; i++) {
+            if (this.storage[i] == null) {
+                this.storage[i] = r;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < this.storage.length; i++) {
+            if (this.storage[i].toString() == uuid) {
+
+                return storage[i];
+            }
+        }
+
         return null;
     }
 
     void delete(String uuid) {
+        int i = 0;
+        for (; i < this.storage.length; i++) {
+            if (this.storage[i].toString() == uuid) {
+                this.storage[i] = null;
+                break;
+            }
+            i++;
+            for (; i < this.storage.length; i++) {
+                this.storage[i - 1] = this.storage[i];
+            }
+        }
     }
 
     /**
